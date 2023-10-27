1ab045370956162edfc2a9a01b865f46325574b9
Alexey Elizarov <felex-ae@ya.ru>
2023-10-27 14:12:32 +0000

    fix: remove old code

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -4,14 +4,12 @@ import { CookiesProvider } from "react-cookie";
 import { WagmiConfig, createConfig, mainnet } from "wagmi";
 import { createPublicClient, http } from "viem";
 
-// import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { WALLET_CONNECT_PROJECT_ID } from "./constants";
 
 const config = createConfig({
   connectors: [
-    // new MetaMaskConnector(),
     new WalletConnectConnector({
       options: {
         projectId: WALLET_CONNECT_PROJECT_ID,
