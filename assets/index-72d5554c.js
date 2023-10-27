72d7c5927db830c28b62846732c6835a1d2ffb75
beautyfree <felex-ae@ya.ru>
2023-10-27 14:09:29 +0000

    Deploying to gh-pages from @ etherean-app/landing@cd5156b1812731d8af3b9a3cb06f11ad5155c951 🚀

--- assets/index-72d5554c.js
+++ assets/index-72d5554c.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as U,a as E,o as ot,R as Do,p as q,y as gt}from"./index-806e397d.js";import"./index-daa3f192.js";/**
+import{n as _t,s as ce,T as N,t as U,a as E,o as ot,R as Do,p as q,y as gt}from"./index-08e304a3.js";import"./index-6c9f8f4a.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
